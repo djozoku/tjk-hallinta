7dc8a1644ddfc0602aa5c6df9f33bd077ef6e577
Ozoku <dragon.djozoku@gmail.com>
2020-03-17 17:09:59 +0200

    chore(workspace): add base configuration files
    Added base configuration files for commitlint, eslint, prettier and typescript.

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+  plugins: ['@typescript-eslint', 'import'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+};
